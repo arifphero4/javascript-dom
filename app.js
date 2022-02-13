2540fdb54a9d67e291c73a562b8d016e7c2652bd
arifphero <ahammadarif232@gmail.com>
2022-02-13 19:04:43 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+const blogTitles = document.getElementsByTagName("h4");
+for (const h4 of blogTitles) {
+  console.log(h4);
+}
+const blogDetails = document.getElementsByTagName("p");
+for (const p of blogDetails) {
+  console.log(p);
+}
+
+const getId = document.getElementById("second-title");
+getId.style.color = "green";
+getId.style.fontSize = "30px";
